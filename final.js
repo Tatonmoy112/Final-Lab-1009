362089a8095a80dc826a4ff5d107187d31205f9a
Tatonmoy112 <tatonmoy112@gmail.com>
2024-12-29 13:52:03 +0600

    Add files via upload

---
+++ final.js
@@ -0,0 +1,6 @@
+function change(){
+    let pChange = document.getElementById("para-1")
+    pChange.innerHTML="DOM Manipulation in Action!"
+    // pChange.style.borderColor="Black"
+    // pChange.style.color="White"
+}
